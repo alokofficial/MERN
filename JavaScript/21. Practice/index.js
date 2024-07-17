411a6e56c0f7a7e473f862df2cc2d23ebec08c28
Alok Kumar <aloknh31@gmail.com>
2024-07-17 19:38:08 +0530

    Day-21

---
+++ JavaScript/21. Practice/index.js
@@ -0,0 +1,38 @@
+// At a restaurant you ate: 1 DAL @100, 2 Rooti @10 each, 1 ICE-Cream @30.
+
+let daal=100;
+let rooti=10;
+let iceCream=30;
+let total=daal*1+rooti*2+iceCream*1;
+console.log(total)
+
+
+// create String using all 3 methods.
+
+let nam="Alok";
+let name1='Alok';
+let name2=`Alok`;
+console.log(name)
+console.log(name1)
+console.log(name2)
+ 
+// concatenate String with Strings, and String with numbers.
+
+// let name="Alok";
+// let name1='Alok';
+// let name2=`Alok`;
+// console.log(name+name1)  
+// console.log(name+1)  
+// console.log(name1+1)  
+// console.log(name2+1) 
+
+// Create Order Summary String for Our Myntra Cart.
+
+let mrp=3895;
+let discountPercentage=45;
+let totalNew=mrp-mrp*(discountPercentage/100);
+let finalTotal=Math.round(totalNew)
+console.log(finalTotal)
+alert(`finalTotal: ${finalTotal}`);
+
+
---
+++ JavaScript/21. Practice/tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+let name="Alok";
+let name1='Alok';
+let name2=`Alok`;
+console.log(name)
+console.log(name1)
+console.log(name2)
