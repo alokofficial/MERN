777ab2d0e79a480165b23e6571527295063b8777
Alok Kumar <aloknh31@gmail.com>
2024-08-02 14:15:40 +0530

    template literals

---
+++ DAY-25/template literals.js
@@ -0,0 +1,17 @@
+let greeting=`Hello, World!`;
+
+console.log(greeting)
+
+let multiLine=`This is a 
+multiline string`;
+
+let firstName="Alok";
+let lastName="Kumar";
+
+console.log(`My name is ${firstName} ${lastName}`)
+
+//Expression Evaluation
+let a=5;
+let b=10;
+
+console.log(`Sum of ${a} and ${b} is ${a+b}`)  ;
