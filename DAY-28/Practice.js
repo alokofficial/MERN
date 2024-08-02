eb84b3c21f3455d39209745274c598e090653831
Alok Kumar <aloknh31@gmail.com>
2024-08-03 01:43:11 +0530

    practice assignment added

---
+++ DAY-28/Practice.js
@@ -0,0 +1,30 @@
+//using continue sum all positive number enter by user and skip negative number
+// let sum = 0;
+// let number = parseInt(prompt("Enter a number: "));
+// while (number !== 0) {
+//     if (number >= 0) {
+//         sum += number;
+//     }
+//     number = parseInt(prompt("Enter a number: "));
+// }
+// console.log(sum)
+
+// print only even number using continue.
+
+// let n=10;
+// for(let i=1;i<=n;i++){
+//     if(i%2!==0){
+//         continue
+//     }
+//     console.log(i)
+// }
+
+/*Write a program that continuously reads integers from the user
+and prints their squares. Use an infinite loop and a break
+statement to exit when a special number (e.g., -1) is entered.*/
+
+// let number = parseInt(prompt("Enter a number: "));
+// while (number !== -1) {
+//     console.log(number * number);
+//     number = parseInt(prompt("Enter a number: "));
+// }   
