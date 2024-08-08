d6cf716366e17e2d1f0a2f95abb94553fd7bc212
Alok Kumar <aloknh31@gmail.com>
2024-08-08 21:14:33 +0000

    changes in the test.js

--- test.js
+++ test.js
@@ -1,12 +1,2 @@
-// define a callback function with normal argument
-function myCallBack(greet){
-    console.log(greet + " how are you!")
-}
-
-// define a higher order function with callback function as argument
-function myHOFunction(callback){
-    let initial="Hi";
-    callback(initial) //call the callback function with argument
-}
-//call the hofunction with callback as callback as argument
-myHOFunction(myCallBack);
+let a=[1,2,3,4,5,6,7,8,9,10];
+a.map((value)=> console.log(value))
