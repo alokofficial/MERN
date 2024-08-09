5b77384538606241d8ed6fbdc2437ebd9e0601df
Alok Kumar <aloknh31@gmail.com>
2024-08-10 01:21:10 +0530

    other folder for interview added

--- test.js
+++ test.js
@@ -1,12 +1,5 @@
-// define a callback function with normal argument
-function myCallBack(greet){
-    console.log(greet + " how are you!")
-}
-
-// define a higher order function with callback function as argument
-function myHOFunction(callback){
-    let initial="Hi";
-    callback(initial) //call the callback function with argument
-}
-//call the hofunction with callback as callback as argument
-myHOFunction(myCallBack);
+for( let i=0;i<3;i++){
+    setTimeout(()=>{
+        console.log(i);
+    },1000);}
+    
