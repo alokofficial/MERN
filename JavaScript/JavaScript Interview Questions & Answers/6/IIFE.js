d6754caafd4539220d34b7404e784b103b69d4e2
Alok Kumar <aloknh31@gmail.com>
2024-08-12 02:30:07 +0530

    removed folder

--- JavaScript/JavaScript Interview Questions & Answers/6/IIFE.js
+++
@@ -1,8 +0,0 @@
-// What is an IIFE (Immediately Invoked Function Expression)?
-
-// An IIFE is a function that is invoked immediately after it is defined.
-(function () {
-  // logic here
-})(); 
-
-//The primary reason to use an IIFE is to obtain data privacy because any variables declared within the IIFE cannot be accessed by the outside world. i.e, If you try to access variables from the IIFE then it throws an error.
