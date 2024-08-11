b54b281971082325db3644d302188ed363e58556
Alok Kumar <aloknh31@gmail.com>
2024-08-12 02:40:08 +0530

    t

--- DAY-28/Accumulator pattern.js
+++
@@ -1,8 +0,0 @@
-// let sum=0;
-// let input;
-// do{
-//     input=parseInt(prompt("Enter a number:"));
-//     if(!isNaN(input))
-//     sum+=input;
-// }while(!isNaN(input))
-// console.log(sum)
--- DAY-33/Objects Equality/using json.js
+++
@@ -1,9 +0,0 @@
-function jsonEquality(obj1, obj2) {
-    return JSON.stringify(obj1) === JSON.stringify(obj2);
-}
-
-const obj1 = { a: 1, b: 2 };
-const obj2 = { a: 1, b: 2 };
-console.log(jsonEquality(obj1, obj2));
-
-//For simple objects without circular references, functions, or undefined values, the JSON.stringify() function can be used.
