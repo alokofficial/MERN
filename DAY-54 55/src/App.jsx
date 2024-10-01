d9e2fa7ff459a754641ac324e827dc8a9b48a010
Alok Kumar <aloknh31@gmail.com>
2024-10-02 01:55:18 +0530

    css modules added

---
+++ DAY-54 55/src/App.jsx
@@ -0,0 +1,9 @@
+import styles from './App.module.css'
+export default function App() {
+  let condition=false
+  return (
+    <>
+      <h1 className={styles.headi}>this is checking css module functionality</h1>
+    </>
+  )
+}
