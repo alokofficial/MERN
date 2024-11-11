969b552ad9d06ac1b63a18df988564b342e00ec6
Alok Kumar <aloknh31@gmail.com>
2024-11-11 05:55:23 +0530

    Day-78 added

---
+++ DAY-78/models/Favourite.js
@@ -0,0 +1,31 @@
+const {getDb} = require('../util/database');
+module.exports = class Favourite {
+
+  static fetchAll() {
+    const db=getDb();
+    return db.collection('favourites').find().toArray().then((favourites) => {
+      return favourites.map(favourite => favourite.homeId.toString());
+    }).catch((error) => {
+      console.log(error);
+    });
+  }
+
+  static addToFavourites(homeId) {
+    const db = getDb();
+    return db.collection('favourites').insertOne({homeId: new ObjectId(String(homeId))}).then((result) => {
+      console.log(result);
+    }).catch((error) => {
+      console.log(error);
+    });
+   
+  }
+
+  static deleteById(removeHomeId) {
+   const db = getDb();
+    return db.collection('favourites').deleteOne({homeId:new ObjectId(String(removeHomeId))}).then((result) => {
+      console.log(result);
+    }).catch((error) => {
+      console.log(error);
+    });
+  }
+}
---
+++ DAY-78/models/Home.js
@@ -0,0 +1,52 @@
+const Favourite = require('./Favourite.js');
+const {getDb} = require('../util/database');
+
+module.exports = class Home {
+  constructor(houseName, price, location, rating, photoUrl, description) {
+    this.houseName = houseName;
+    this.price = price;
+    this.location = location;
+    this.rating = rating;
+    this.photoUrl = photoUrl;
+    this.description = description;
+  }
+  save() {
+    const db=getDb();
+    if(this.id) {
+      return db.collection('homes').updateOne({_id:ObjectId(String(this.id))}, {$set: this}).then((result) => {
+        console.log(result);
+      });
+    }else{
+      return db.collection('homes').insertOne(this).then((result) => {
+        console.log(result);
+      });
+    }
+  }
+
+  
+  static fetchAll() {
+   const db=getDb();
+    return db.collection('homes').find().toArray().then((homes) => {
+      return homes;
+    }).catch((error) => {
+      console.log(error);
+    });
+  }
+
+  static findById(homeId) {
+    const db=getDb();
+    return db.collection('homes').find({_id:ObjectId(String(homeId))}).next().then((homes) => {
+      return homes;
+    }).catch((error) => {
+      console.log(error);
+    });
+  }
+ 
+  static deleteById(homeId) {
+    const db=getDb();
+    return db.collection('homes').deleteOne({_id:ObjectId(String(homeId))}).then((result) => {
+      console.log(result);
+    });
+    
+  }
+}
